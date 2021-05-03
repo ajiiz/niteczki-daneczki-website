fcedb6d2a0b0c5d93c882d38339c3b44da2cf6c7
ajiiz <opticpiter@gmail.com>
2021-05-03 16:28:02 +0200

    Added Homepage elements

--- niteczki-daneczki-website/src/Components/Home.js
+++ niteczki-daneczki-website/src/Components/Home.js
@@ -1,9 +1,31 @@
 import React from "react"
 
+import Flowers from "../images/undraw_flowers_vx06.svg"
+import { ArrowDownIcon } from '@heroicons/react/solid'
+
+import "../styles/home.scss"
+
 const Home = () => {
     return (
-        <div>
-            Homepage
+        <div className="home">
+            <div className="header">
+                <div className="content">
+                    <h1 className="content__header">szydełkowa pasja</h1>
+                    <h3 className="content__info">
+                        <span className="content__info__line">Łącze przyjemne z pożytecznym tworząc piękne rzeczy.</span>
+                        <span className="content__info__line">Szydełkuję na zamówienie i jestem otwarta na propozycję.</span>
+                        <span className="content__info__line">Gorąco zapraszam do kontaktu.</span>
+                    </h3>
+                    <button className="content__contact">
+                        kontakt
+                    </button>
+                </div>
+                <img className="header-image" src={Flowers}></img>
+            </div>
+            <div className="scroll-down">
+                <p className="scroll-down__text">przejdź dalej</p>
+                <ArrowDownIcon className="scroll-down__btn" />
+            </div>
         </div>
     )
 }
