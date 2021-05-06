4087b8a860e8d73917f1f85893d2c729a71e3219
ajiiz <opticpiter@gmail.com>
2021-05-06 23:17:37 +0200

    Added Aboutpage elements

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
 import React from "react"
 
+import Work from "../images/undraw_Playful_cat_rchv.svg"
+import Send from "../images/undraw_Gift_box_re_vau4.svg"
+import Review from "../images/undraw_reviews_lp8w.svg"
+
 import "../styles/about.scss"
 
 const About = () => {
     return (
         <div className="about">
-            About Section
+            <ul className="about-list">
+                <li className="about-list__item">
+                    <img className="about-list__item-img" src={Work}></img>
+                    <h2 className="about-list__item-header">Praca</h2>
+                    <p className="about-list__item-content">Szybko i dokładnie wykonuję szydełkowe zlecenia.</p>
+                </li>
+                <li className="about-list__item">
+                    <img className="about-list__item-img" src={Send}></img>
+                    <h2 className="about-list__item-header">Cena i wysłanie</h2>
+                    <p className="about-list__item-content">Cena zależna od projektu jest do ustalenia. Wysyłam paczki w każde zakątki świata.</p>
+                </li>
+                <li className="about-list__item">
+                    <img className="about-list__item-img" src={Review}></img>
+                    <h2 className="about-list__item-header">Ocena</h2>
+                    <p className="about-list__item-content">Posiadam wiele zadowolonych klientek z całego świata.</p>
+                </li>
+            </ul>
         </div>
     )
 }
