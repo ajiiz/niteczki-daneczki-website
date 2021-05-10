67f97b929ba5ef372280105d6f4196967519d7d4
ajiiz <opticpiter@gmail.com>
2021-05-10 17:37:31 +0200

    Added Slider logic - functions to change slides

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,15 +10,30 @@ const Slider = () => {
     const [current, setCurrent] = useState(0)
     const length = SliderData.length
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
     return (
         <div className="slider">
-            <ArrowNarrowLeftIcon className="icon"/>
-            <ArrowNarrowRightIcon className="icon"/>
+            <ArrowNarrowLeftIcon className="icon" onClick={prevSlide} />
             {SliderData.map((data, key) => {
                 return (
-                    <img className="slider__img" src={data.image} key={key} alt="gallery" />
+                    <div
+                        className={key === current ? "slide active" : "slide"}
+                        key={key}
+                    >
+                        {key === current && (
+                            <img className="slider-img" src={data.image} key={key} alt="gallery" />
+                        )}
+                    </div>
                 )
             })}
+            <ArrowNarrowRightIcon className="icon" onClick={nextSlide} />
         </div>
     )
 }
