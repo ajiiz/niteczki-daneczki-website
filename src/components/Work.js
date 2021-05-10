e4690631d0a84aafd01dac21251ed84da7402716
ajiiz <opticpiter@gmail.com>
2021-05-10 16:33:58 +0200

    Added Work component elements

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,7 +7,19 @@ import "../styles/work.scss"
 const Work = () => {
     return (
         <div className="work">
-            Workpage
+            <Slider />
+            <div className="content">
+                <h1 className="content__header">galeria prac</h1>
+                <h3 className="content__info">
+                    <span className="content__info__line">Szukam wyzwań i z chęcią podejmę się nowych projektów.</span>
+                    <span className="content__info__line">Więcej moich prac można zobaczyć na instagramie.</span>
+                </h3>
+                <a href="https://www.instagram.com/niteczkidaneczki/" target="_blank" rel="noreferrer">
+                    <button className="content__contact">
+                        zobacz
+                    </button>
+                </a>
+             </div>
         </div>
     )
 }
