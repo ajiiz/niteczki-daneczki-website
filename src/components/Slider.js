2e3960dee7692d0f4abe2187884e7947149f628c
ajiiz <opticpiter@gmail.com>
2021-05-10 16:54:54 +0200

    Imported SliderData and showed all images

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,17 @@
 import React from "react"
 
+import { SliderData } from "./SliderData"
+
 import "../styles/slider.scss"
 
 const Slider = () => {
     return (
         <div className="slider">
-            s
+            {SliderData.map((data, key) => {
+                return (
+                    <img src={data.image} key={key} alt="gallery" />
+                )
+            })}
         </div>
     )
 }
