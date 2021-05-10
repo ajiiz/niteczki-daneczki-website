a4338ab8e864f8e6501f7f22938742067ba42f36
ajiiz <opticpiter@gmail.com>
2021-05-10 16:55:15 +0200

    Modified Slider texts

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,6 +12,7 @@ const Work = () => {
                 <h1 className="content__header">galeria prac</h1>
                 <h3 className="content__info">
                     <span className="content__info__line">Szukam wyzwań i z chęcią podejmę się nowych projektów.</span>
+                    <span className="content__info__line">Wszystko wykonane jest starannie i własnoręcznie przeze mnie.</span>
                     <span className="content__info__line">Więcej moich prac można zobaczyć na instagramie.</span>
                 </h3>
                 <a href="https://www.instagram.com/niteczkidaneczki/" target="_blank" rel="noreferrer">
