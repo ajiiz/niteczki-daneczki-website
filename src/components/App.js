26dc070a5c476d92527814014c2ebe812d81bacb
ajiiz <opticpiter@gmail.com>
2021-05-18 19:28:05 +0200

    Added state that shows which language is set

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,19 @@ import Pages from "./Pages"
 const App = () => {
 
   const [isSidebar, setIsSidebar] = useState(false);
+  const [isPolish, setIsPolish] = useState(true);
 
   const handleSidebar = () => {
     setIsSidebar(!isSidebar)
   }
 
+  const handleLanguage = () => {
+    setIsPolish(!isPolish)
+  }
+
   return (
     <div className="main">
-      <Sidebar isSidebar={isSidebar} handleSidebar={handleSidebar}/>
+      <Sidebar isSidebar={isSidebar} handleSidebar={handleSidebar} handleLanguage={handleLanguage} isPolish={isPolish}/>
       <Pages isSidebar={isSidebar}/>
     </div>
   )
