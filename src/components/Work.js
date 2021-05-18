6ecd35d1595ba2a94b50b0e593ed3b7efd798943
ajiiz <opticpiter@gmail.com>
2021-05-18 20:30:47 +0200

    Added content-data to workpage

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,20 +4,20 @@ import Slider from "./Slider"
 
 import "../styles/work.scss"
 
-const Work = () => {
+const Work = ({ content }) => {
     return (
         <div className="work">
             <Slider />
             <div className="content">
-                <h1 className="content__header">galeria prac</h1>
+                <h1 className="content__header">{content.title}</h1>
                 <h3 className="content__info">
-                    <span className="content__info__line">Szukam wyzwań i z chęcią podejmę się nowych projektów.</span>
-                    <span className="content__info__line">Wszystko wykonane jest starannie i własnoręcznie przeze mnie.</span>
-                    <span className="content__info__line">Więcej moich prac można zobaczyć na instagramie.</span>
+                    <span className="content__info__line">{content.content[0]}</span>
+                    <span className="content__info__line">{content.content[1]}</span>
+                    <span className="content__info__line">{content.content[2]}</span>
                 </h3>
                 <a href="https://www.instagram.com/niteczkidaneczki/" target="_blank" rel="noreferrer">
                     <button className="content__contact">
-                        zobacz
+                        {content.btn}
                     </button>
                 </a>
              </div>
