e60c84522b1823770742e8d82d0e3441da315e0b
ajiiz <opticpiter@gmail.com>
2021-05-21 23:26:07 +0200

    Imported gallery-images to SliderData

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,14 +1,19 @@
+import hearts from "../images/hearts.jpg"
+import ribbons from "../images/ribbons.jpg"
+import bauble from "../images/bauble.jpg"
+import goose from "../images/goose.jpg"
+
 export const SliderData = [
     {
-        image: "https://images.unsplash.com/photo-1470049384172-927891aad5e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+        image: ribbons
     },
     {
-        image: "https://images.unsplash.com/photo-1530396333989-24c5b8f805dd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+        image: hearts
     },
     {
-        image: "https://images.unsplash.com/photo-1532629804872-98086b058cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
+        image: bauble
     },
     {
-        image: "https://images.unsplash.com/photo-1576437674613-ea447862afe8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+        image: goose
     }
 ]
